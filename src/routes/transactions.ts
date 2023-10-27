0763141bdacd2471d2ccb053435bd59ac683b602
Matheus Gabriel <matheusgmello1@gmail.com>
2023-10-27 14:05:46 -0300

    Feat: Implementing cookies in Fastify

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -11,15 +11,17 @@ export async function transactionsRoutes(app: FastifyInstance) {
   })
 
   app.get('/:id', async (request) => {
-    const getTransactionParamsSchema = z.object({
+    const getTransactionsParamsSchema = z.object({
       id: z.string().uuid(),
     })
 
-    const { id } = getTransactionParamsSchema.parse(request.params)
+    const { id } = getTransactionsParamsSchema.parse(request.params)
 
     const transaction = await knex('transactions').where('id', id).first()
 
-    return { transaction }
+    return {
+      transaction,
+    }
   })
 
   app.get('/summary', async () => {
@@ -41,10 +43,22 @@ export async function transactionsRoutes(app: FastifyInstance) {
       request.body,
     )
 
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
 
     return reply.status(201).send()
